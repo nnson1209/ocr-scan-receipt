82676e63b86420098305164105e4e7a5ded36c56
nnson1209 <nnson120924@gmail.com>
2025-10-03 13:17:55 +0700

    fix log

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -8,10 +8,10 @@ export const logger = (req: Request, res: Response, next: NextFunction) => {
     const originalEnd = res.end;
 
     // Override res.end with a simple wrapper
-    res.end = function(this: Response, chunk?: any, encoding?: any, cb?: any) {
+    res.end = function (this: Response, chunk?: any, encoding?: any, cb?: any) {
         const duration = Date.now() - start;
         console.log(`${req.method} ${req.originalUrl} - ${res.statusCode} - ${duration}ms`);
-        
+
         // Call the original end method with all arguments
         return originalEnd.call(this, chunk, encoding, cb);
     } as any;
